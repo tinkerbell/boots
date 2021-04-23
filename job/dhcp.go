a40a33a3317cd85bf9cfbdc834b798be43fec42d
Manuel Mendez <mmendez@equinix.com>
2021-04-23 15:35:19 +0000

    Drop Packet from iPXE version checks and use
    Just getting rid of a Packetism.
    
    Signed-off-by: Manuel Mendez <mmendez@equinix.com>

--- job/dhcp.go
+++ job/dhcp.go
@@ -69,12 +69,12 @@ func (j Job) configureDHCP(rep, req *dhcp4.Packet) bool {
 			j.With("dhcp", isUEFI, "job", j.IsUEFI()).Info("uefi mismatch, using dhcp")
 		}
 
-		isPacket := ipxe.IsPacketIPXE(req)
-		if isPacket {
+		isOuriPXE := ipxe.IsOuriPXE(req)
+		if isOuriPXE {
 			ipxe.Setup(rep)
 		}
 
-		j.setPXEFilename(rep, isPacket, isARM, isUEFI)
+		j.setPXEFilename(rep, isOuriPXE, isARM, isUEFI)
 	}
 	return true
 }
@@ -97,7 +97,7 @@ func (j Job) areWeProvisioner() bool {
 	return j.hardware.HardwareProvisioner() == j.ProvisionerEngineName()
 }
 
-func (j Job) setPXEFilename(rep *dhcp4.Packet, isPacket, isARM, isUEFI bool) {
+func (j Job) setPXEFilename(rep *dhcp4.Packet, isOuriPXE, isARM, isUEFI bool) {
 	if j.HardwareState() == "in_use" {
 		if j.InstanceID() == "" {
 			j.Error(errors.New("setPXEFilename called on a job with no instance"))
@@ -129,7 +129,7 @@ func (j Job) setPXEFilename(rep *dhcp4.Packet, isPacket, isARM, isUEFI bool) {
 
 	var filename string
 	var pxeClient bool
-	if !isPacket {
+	if !isOuriPXE {
 		if j.PArch() == "hua" || j.PArch() == "2a2" {
 			filename = "snp-hua.efi"
 		} else if isARM {
